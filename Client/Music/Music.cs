8c52cc93444579399988496d11f131addb3e6719
JulianG97 <julian.gamauf@fhwn.ac.at>
2017-10-22 17:29:51 +0200

    Music Update

---
+++ Client/Music/Music.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+using System.IO;
+
+namespace Client
+{
+    public class Music
+    {
+        private WindowsMediaPlayer musicPlayer;
+        private WindowsMediaPlayer soundPlayer;
+
+        public Music()
+        {
+            musicPlayer = new WindowsMediaPlayer();
+            soundPlayer = new WindowsMediaPlayer();
+        }
+
+        public void PlayMusic(string song)
+        {
+            musicPlayer.settings.setMode("loop", true);
+            musicPlayer.URL = GetMusicDirectory() + song + ".mp3";
+            musicPlayer.controls.play();
+        }
+
+        public void StopMusic()
+        {
+            musicPlayer.controls.stop();
+        }
+
+        public void PlaySound(string sound)
+        {
+            soundPlayer.URL = GetMusicDirectory() + sound + ".mp3";
+            soundPlayer.controls.play();
+        }
+
+        private string GetMusicDirectory()
+        {
+            string path = Directory.GetCurrentDirectory();
+            string[] pathArray = path.Split('\\');
+
+            path = pathArray[0];
+
+            for (int i = 1; i < pathArray.Length; i++)
+            {
+                path = path + "\\" + pathArray[i];
+
+                if (pathArray[i] == "Client")
+                {
+                    break;
+                }
+            }
+
+            return path + "\\Music\\";
+        }
+    }
+}
